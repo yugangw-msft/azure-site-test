a29a6991289e572def4a5556c2b79d9d40f39402
yugangw-msft <yugangw@microsoft.com>
2017-10-09 10:47:28 -0700

    add msi code

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var server = app.listen(port,function(){
+var request = require('request');
+var msiEndpoint = process.env.MSI_ENDPOINT;
+var msiSecret = process.env.MSI_SECRET;
+
+var server = app.listen(port, function (){
     console.log("We have started our server on port 3000");
 });
 
 app.get('/', function (req, res) {
-  var hostname = req.headers.host.split(":")[0];
-
-  if(hostname.startsWith("admin."))
-    res.send("this is admin response!");
-  else {
-    console.log('PORT is:' + process.env.PORT)
-    console.error("Something wrong");
-    console.log("Something normal");
-    res.send('Hello world 4 using Node ' + process.version);
+  if (!msiEndpoint || !msiSecret) {
+    res.send('MSI is not configured!');
+    return;
   }
+  var tokenUri = msiEndpoint + '/resource=https://management.azure.com/&api-version=2017-09-01';
+  var options = {
+    url: tokenUri,
+    headers: {
+      Secret: msiSecret
+    }
+  };
+  request(tokenUri, function (error, response, body) {
+    if (error) {
+      res.send('MSI token request failed: ' + error);
+      return;
+    }
+    res.send('Token:' + JSON.stringify(response));
+  });
 });
