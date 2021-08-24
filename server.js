43ebaf56763d308d55dc44e9f845d8012db5f63e
Yugang Wang <yugangw@microsoft.com>
2021-08-23 21:15:15 -0700

    update code with status code and echo text

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
-var server = app.listen(port,function(){
-    console.log("We have started our server on port 3000");
+var server = app.listen(port, function () {
+  console.log("We have started our server on port 3000");
 });
 
 app.get('/', function (req, res) {
-  var hostname = req.headers.host.split(":")[0];
-
-  if(hostname.startsWith("admin."))
-    res.send("this is admin response!");
-  else {
-    console.log('PORT is:' + process.env.PORT)
-    console.error("Something wrong");
-    console.log("Something normal");
-    res.send('Hello world 4 using Node ' + process.version);
+  var status = 200;
+  console.log('Using Node ' + process.version);
+  if (req.query.status) {
+    status = Number(req.query.status)
   }
+  if (status >= 400) {
+    console.error("Something wrong");
+  }
+  else {
+    console.log("Something normal");
+  }
+  var echo = req.query.echo;
+  res.status(status);
+  res.send(echo + " at " + (new Date().toLocaleString()));
 });
